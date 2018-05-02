d45c44d4224a089fd91e0c39158281abf5d4ac10
gpelicant <gpelicant@tradelab.fr>
2018-05-02 11:38:15 +0200

    add Oauth

--- settings/index.js
+++ settings/index.js
@@ -1,5 +1,6 @@
 module.exports = (api) => {
     api.settings = require('./settings.json');
+    api.clientSecret = require('./client_secret.json');
 
     if (process.env.NODE_ENV == 'production') {
         api.settings.port = process.env.PORT;
