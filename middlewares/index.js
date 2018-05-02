1da6c46cbe9792f5fac156db7b318c945c4fdbfa
yoannKy <y.keoxay@gmail.com>
2018-05-02 10:52:57 +0200

    initial commit

---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+module.exports = (server) => {
+    server.middlewares = {
+        bodyParser: require('body-parser'),
+        checkMandatoryBodyFields: require('./checkMandatoryBodyFields'),
+        checkUnwantedBodyFields: require('./checkUnwantedBodyFields'),
+        logger: require('./logger'),
+        res: require('./res'),
+    };
+};
